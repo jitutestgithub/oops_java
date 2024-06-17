89afb6bc5b7954d217714dceefe3c2f6b487fb6e
Jitendra Kumar Singh <js7068346592@gmail.com>
2024-06-17 10:59:33 +0530

    oops_java.iml

--- src/first_1.java
+++ src/first_1.java
@@ -1,2 +1,16 @@
 public class first_1 {
+
+    public void eat(){
+        System.out.println("dogs food eting");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        first_1 dogs=new first_1();
+        dogs.eat();
+        dogs.run();
+    }
+    public void run(){
+        System.out.println("dogs running");
+    }
 }
