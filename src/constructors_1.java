feb3ac09aa3fd0607e0cf4d5e7266ff286c07216
Jitendra Kumar Singh <js7068346592@gmail.com>
2024-06-17 11:33:49 +0530

    oops_java.iml

---
+++ src/constructors_1.java
@@ -0,0 +1,13 @@
+public class constructors_1 {
+
+    constructors_1(){
+        System.out.println("lp");
+    }
+
+
+
+    public static void main(String[] args) {
+        constructors_1 dogs=new constructors_1();
+
+    }
+}
